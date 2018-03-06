2586359b1470238502d5b3b1c8ba37d9377a717b
Didier Zuñiga <didier35@hotmail.com>
2018-03-05 22:24:28 -0500

    [add] First commit

---
+++ variance.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+def run():
+	n = int(input("¿Cuantos datos deseas ingresar? -> "))
+	data = []
+	sumData = 0
+	sumresults = 0
+	for x in range(n):
+		inputs = int(input("Ingresa dato {}: ".format(x+1)))
+		sumData += inputs
+		data.insert(x,inputs)
+	medium = sumData / len(data)
+	for i in range(n):
+		data[i] = data[i]-medium
+	for j in range(n):
+		sumresults += data[j]**2
+		data[j] = data[j]**2
+	print("La varianza es: {}".format(sumresults / len(data)))
+	
+
+if __name__ == '__main__':
+	run()
